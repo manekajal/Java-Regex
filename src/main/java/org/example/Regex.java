b8beebaad413827fcc5f3d270fff197b75da7678
kajal <manekajal2014@gmail.com>
2021-10-06 13:50:39 +0530

    [Kajal] Refactored Java Regex

--- src/main/java/org/example/Regex.java
+++ src/main/java/org/example/Regex.java
@@ -1,7 +1,24 @@
 package org.example;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Regex {
     public static void main(String[] args){
-        System.out.println(" Java Regex :");
+        String line="Kaj";
+        String pattern= "^[A-z]{3}";
+        Pattern r= Pattern.compile(pattern);
+
+        Matcher m = r.matcher(line);
+
+        if(m.matches()){
+            System.out.println("Matched Pattern:");
+
+        }else
+        {
+            System.out.println(" No Matched :");
+        }
+
+
     }
 }
